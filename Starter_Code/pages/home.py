051b05d963eaa68bd191ca33c574a0d5b678cb42
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-09-02 19:53:50 -0700

    Updates.

--- Starter_Code/pages/home.py
+++ Starter_Code/pages/home.py
@@ -3,5 +3,7 @@
 import streamlit as st
 
 def app():
-    st.title('TimeLockWallet')
-    st.write("Welcome to TimeLockWallet. Navigate using the sidebar.")
+    st.title('AstroFusion Wallet 🌌')
+    st.write("""
+    Welcome to AstroFusion Wallet, your trusted spacecraft in the financial universe. Navigate the complex orbits of stock predictions, secure your treasures with TimeLock functionalities, and explore the limitless possibilities of cryptocurrency.
+    """)
