d3a3a438531d3411138e6c290a7093c413a0f6c9
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-09-02 19:35:50 -0700

    Updates.

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,11 +1,12 @@
 # app.py
 
 import streamlit as st
-from pages import home, admin_dashboard, transaction_history, help_support
+from pages import home, admin_dashboard, transaction_history, help_support, user_dashboard
 
 PAGES = {
     "Home": home,
     "Admin Dashboard": admin_dashboard,
+    "User Dashboard": user_dashboard,
     "Transaction History": transaction_history,
     "Help & Support": help_support
 }
